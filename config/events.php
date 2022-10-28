cfde4f67220890d202b09dac09b9dccab5156695
Chris Yates <chris@clyates.plus.com>
2022-10-28 10:25:53 +0100

    Add config/events.php

---
+++ config/events.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @copyright Copyright © 2022 BeastBytes - All rights reserved
+ * @license BSD 3-Clause
+ */
+
+declare(strict_types=1);
+
+use BeastBytes\SchemaDotOrg\SchemaDotOrg;
+use Yiisoft\View\Event\WebView\BodyEnd;
+
+return [];
